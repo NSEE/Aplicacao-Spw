c6696de4aa4957cefbe424f55ed080290d487e51
Dennis Teles <dennis.tds@hotmail.com>
2015-11-16 21:56:25 -0200

    Trabalhando no loopback com novo codec SpW

--- Materiais/Codigos/Xilinx/Testes/Teste_loopback_SpW/Codec_SpW_loopback/loopback_SpW.vhd
+++ Materiais/Codigos/Xilinx/Testes/Teste_loopback_SpW/Codec_SpW_loopback/loopback_SpW.vhd
@@ -20,6 +20,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use work.spwpkg.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -85,10 +86,62 @@ architecture Behavioral of loopback_SpW is
                                                             -- 2 --> '1'= "Escape Error"      | 7 --> '1'= "Started"
                                                             -- 3 --> '1'= "Credit Error"      | 8 --> '1'= "Connecting"
                                                             -- 4 --> '1'= "ErrorReset"        | 9 -> '1'= "Run"
-
         );
     END COMPONENT;
 	 -------------------------------------------------------------------------------------------------------------
+
+	-- Component Declaration for the Unit Under Test 2 (UUT2)
+    COMPONENT spwstream
+	generic (
+        sysfreq:        real;
+        txclkfreq:      real := 0.0;
+        rximpl:         spw_implementation_type := impl_generic;
+        rxchunk:        integer range 1 to 4 := 1;
+        tximpl:         spw_implementation_type := impl_generic;
+        rxfifosize_bits: integer range 6 to 14 := 11;
+        txfifosize_bits: integer range 2 to 14 := 11
+    );
+
+    port (
+        clk:        in  std_logic;
+        rxclk:      in  std_logic;
+        txclk:      in  std_logic;
+        rst:        in  std_logic;
+        autostart:  in  std_logic;
+        linkstart:  in  std_logic;
+        linkdis:    in  std_logic;
+        txdivcnt:   in  std_logic_vector(7 downto 0);
+        tick_in:    in  std_logic;
+        ctrl_in:    in  std_logic_vector(1 downto 0);
+        time_in:    in  std_logic_vector(5 downto 0);
+        txwrite:    in  std_logic;
+        txflag:     in  std_logic;
+        txdata:     in  std_logic_vector(7 downto 0);
+        txrdy:      out std_logic;
+        txhalff:    out std_logic;
+        tick_out:   out std_logic;
+        ctrl_out:   out std_logic_vector(1 downto 0);
+        time_out:   out std_logic_vector(5 downto 0);
+        rxvalid:    out std_logic;
+        rxhalff:    out std_logic;
+        rxflag:     out std_logic;
+        rxdata:     out std_logic_vector(7 downto 0);
+        rxread:     in  std_logic;
+        started:    out std_logic;
+        connecting: out std_logic;
+        running:    out std_logic;
+        errdisc:    out std_logic;
+        errpar:     out std_logic;
+        erresc:     out std_logic;
+        errcred:    out std_logic;
+        spw_di:     in  std_logic;
+        spw_si:     in  std_logic;
+        spw_do:     out std_logic;
+        spw_so:     out std_logic
+    );
+    END COMPONENT;
+	 -------------------------------------------------------------------------------------------------------------
+
 
 	 -- Component LVDS Outputs
 	 COMPONENT OBUFDS PORT(
@@ -168,6 +221,44 @@ architecture Behavioral of loopback_SpW is
 	signal s_GPIOs_i : std_logic_vector(2 downto 0) := (OTHERS => '0');
 	signal s_GPIOs_o : std_logic_vector(5 downto 3) := (OTHERS => '0');
 	--------------------------------------------------------------------------------------
+	
+	
+	-- Signal SpW light
+	signal sig_autostart : std_logic;
+	signal sig_linkstart : std_logic;
+	signal sig_linkdis   : std_logic;
+	
+	signal sig_rst      : std_logic;
+	signal sig_tick_in  : std_logic;
+    signal sig_ctrl_in  : std_logic_vector(1 downto 0);
+	signal sig_time_in  : std_logic_vector(5 downto 0) := (others => '0');
+	signal sig_txwrite  : std_logic;
+	signal sig_txflag   : std_logic;
+	signal sig_txdata   : std_logic_vector(7 downto 0) := (others => '0');
+	signal sig_txrdy    : std_logic;
+	signal sig_txhalff  : std_logic;
+	signal sig_tick_out : std_logic;
+	signal sig_ctrl_out	: std_logic_vector(1 downto 0);
+	signal sig_time_out	: std_logic_vector(5 downto 0);
+	signal sig_rxvalid  : std_logic;
+	signal sig_rxhalff  : std_logic;
+	signal sig_rxflag   : std_logic;
+	signal sig_rxdata   : std_logic_vector(7 downto 0);
+	signal sig_rxread   : std_logic;
+	signal sig_started  : std_logic;
+	signal sig_connecting : std_logic;
+	signal sig_running  : std_logic;
+	signal sig_errdisc  : std_logic;
+	signal sig_errpar   : std_logic;
+	signal sig_erresc   : std_logic;
+	signal sig_errcred  : std_logic;
+	signal sig_spw_di   : std_logic;
+	signal sig_spw_si   : std_logic;
+	signal sig_spw_do   : std_logic;
+	signal sig_spw_so   : std_logic;
+	
+	signal codec_state : std_logic_vector(6 downto 0);
+	
 begin
 
 --Clk <= CLOCK;
@@ -213,56 +304,158 @@ begin
         );
 
 
+	c_SpW_light: spwstream
+	generic map(
+        sysfreq		=> 100.0e6,
+        txclkfreq	=> 100.0e6,
+        rximpl		=> impl_generic,
+        rxchunk		=> 1,
+        tximpl		=> impl_generic,
+        rxfifosize_bits => 11,
+        txfifosize_bits => 11
+    )
+    port map(
+        clk 	=> Clk,
+        rxclk	=> Clk,
+        txclk	=> Clk,
+        rst		=> not(RESET_doubleclk),
+        autostart 	=> sig_autostart,
+        linkstart	=> sig_linkstart,
+        linkdis		=> sig_linkdis,
+        txdivcnt	=> "00000001",
+        tick_in		=> sig_tick_in,
+        ctrl_in		=> sig_ctrl_in,
+        time_in		=> sig_time_in,
+        txwrite		=> '1',
+        txflag		=> sig_txflag,
+        txdata		=> sig_txdata,
+        txrdy		=> sig_txrdy,
+        txhalff		=> sig_txhalff,
+        tick_out	=> sig_tick_out,
+        ctrl_out	=> sig_ctrl_out,
+        time_out	=> sig_time_out,
+        rxvalid		=> sig_rxvalid,
+        rxhalff		=> sig_rxhalff,
+        rxflag		=> sig_rxflag,
+        rxdata		=> sig_rxdata,
+        rxread		=> sig_rxread,
+        started		=> sig_started,
+        connecting	=> sig_connecting,
+        running		=> sig_running,
+        errdisc		=> sig_errdisc,
+        errpar		=> sig_errpar,
+        erresc		=> sig_erresc,
+        errcred		=> sig_errcred,
+        spw_di		=> sig_spw_di,
+        spw_si		=> sig_spw_si,
+        spw_do		=> sig_spw_do,
+        spw_so		=> sig_spw_so
+    );
 	
-MReset      <= '0';	
-LinkStart1    <= '1';
-LinkDisable1  <= '0';
-AutoStart1    <= '1';
+	
+-- =========================================
+-- * * * * * Inicializando conexao * * * * *
+-- =========================================
+
+--MReset      <= '0';	
+--LinkStart1    <= '1';
+--LinkDisable1  <= '0';
+--AutoStart1    <= '1';
+
+	sig_autostart <= '1';
+	sig_linkstart <= '1';
+	sig_linkdis <= '0';
+	
+-- =========================================
+
+-- =========================================
+-- * * Transformar status codec em vetor * *
+-- =========================================
+	codec_state <=  sig_started & 
+					sig_connecting & 
+					sig_running & 
+					sig_errdisc & 
+					sig_errpar & 
+					sig_erresc & 
+					sig_errcred;
+-- =========================================
+
 
 s_GPIOs_i <= GPIOs_i;
 GPIOs_o <= s_GPIOs_o;
 
--- Exibir dados do RX_Data1 nos leds da placa Filha
-LEDs_PF(7) <= RX_Data1(7);
-LEDs_PF(6) <= RX_Data1(6);
-LEDs_PF(5) <= RX_Data1(5);
-LEDs_PF(4) <= RX_Data1(4);
-LEDs_PF(3) <= RX_Data1(3);
-LEDs_PF(2) <= RX_Data1(2);
-LEDs_PF(1) <= RX_Data1(1);
-LEDs_PF(0) <= RX_Data1(0);
+
+-- ==========================================
+-- * * * *  Exibir rxdata nos leds  * * * * *
+-- ==========================================
+
+---- Exibir dados do RX_Data1 nos leds da placa Filha
+--LEDs_PF(7) <= RX_Data1(7);
+--LEDs_PF(6) <= RX_Data1(6);
+--LEDs_PF(5) <= RX_Data1(5);
+--LEDs_PF(4) <= RX_Data1(4);
+--LEDs_PF(3) <= RX_Data1(3);
+--LEDs_PF(2) <= RX_Data1(2);
+--LEDs_PF(1) <= RX_Data1(1);
+
+LEDs_PF(0) <= sig_rxdata(0);
+LEDs_PF(7) <= sig_rxdata(7);
+LEDs_PF(6) <= sig_rxdata(6);
+LEDs_PF(5) <= sig_rxdata(5);
+LEDs_PF(4) <= sig_rxdata(4);
+LEDs_PF(3) <= sig_rxdata(3);
+LEDs_PF(2) <= sig_rxdata(2);
+LEDs_PF(1) <= sig_rxdata(1);
+LEDs_PF(0) <= sig_rxdata(0);
+
+-- =========================================
+
+
+-- ==========================================
+-- * * * *  loopback SpaceWire light  * * * * *
+-- ==========================================
+    sig_spw_di <= sig_spw_do;
+    sig_spw_si <= sig_spw_so;
+-- =========================================	
+
 
 -- Buffer_Write1 no GPIO_o
 s_GPIOs_o(3) <= Buffer_Write1;
 
 
-
--- PROCESSO CODEC SpW-=-=-=-==--=-=-=-=-=-=-=--=-=-=--=-==-=-=-=-=-=-=-=--=-=-=-=-
+-- ===============================================================================================
+-- = = = = = = = = = = = = = =   PROCESSO LOOPBACK CODEC SpW   = = = = = = = = = = = = = = = = = = 
+-- ===============================================================================================
 	PROCESS (Clk)
 	begin
 		if	(rising_edge(Clk)) then
 		
-			if (EstadoInterno1(9) = '0') then
+			if (codec_state(4) = '0') then -- Se codec SpW NÃO está em estado de running...
 				estado_codec1 <= estado_desativado1;
-			else
-				Tick_in1 <= Tick_out1;
-				Time_in1 <= Time_out1;
+			else -- Se está em estado de running...
+--				Tick_in1 <= Tick_out1;
+--				Time_in1 <= Time_out1;
+				sig_tick_in <= sig_tick_out;
+				sig_time_in <= sig_time_out;
 				case estado_codec1 is 
 					when estado_desativado1 =>
-						if Buffer_Write1 = '1' then --Pode ler?
+--						if Buffer_Write1 = '1' then --Pode ler?
+						if sig_rxvalid = '1' then -- Tem dado para ler?
 							estado_codec1 <= estado_leitura1; -- Vai para o estado de leitura.
-						else
+						else -- Se não tem dado para ler, então espera...
 							estado_codec1 <= estado_desativado1;-- Espera permissao para leitura...
 						end if;
 					when estado_leitura1 =>
-						if (Buffer_Write1 = '0') and (TX_Ready1 = '1') then --Terminou a leitura e pode escrever?
-							TX_data1 <= RX_data1;
+--						if (Buffer_Write1 = '0') and (TX_Ready1 = '1') then --Terminou a leitura e pode escrever?
+						if (sig_rxvalid = '0') and (sig_txrdy = '1') then -- Terminou a leitura e pode escrever?
+--							TX_data1 <= RX_data1;
 							estado_codec1 <= estado_escreve1; -- Vai para o estado de escrita.
 						else
 							estado_codec1 <=  estado_leitura1;
 						end if;	
 					when estado_escreve1 =>
-						if (TX_Ready1 = '1') then
+--						if (TX_Ready1 = '1') then
+						if sig_txrdy = '1' then
 							estado_codec1 <= estado_escreve1; -- Continua em estado de escrita até terminar (TX_Ready1 = '0').
 						else
 							estado_codec1 <= estado_desativado1;
@@ -279,18 +472,31 @@ s_GPIOs_o(3) <= Buffer_Write1;
         case estado_codec1 is
 		
             when estado_desativado1 => 
-                TX_Write1 <= '0';
-				Buffer_Ready1 <= '1'; -- Pede pra realizar leitura.
+--              TX_Write1 <= '0';
+--				Buffer_Ready1 <= '1'; -- Pede pra realizar leitura.
+				sig_txwrite <= '0'; -- Nao escreve
+				sig_rxread <= '0'; -- Espera para poder ler
             when estado_leitura1 =>
-                TX_Write1 <= '0';
-				Buffer_Ready1 <= '0'; -- Realiza a leitura.	 
+--              TX_Write1 <= '0';
+--				Buffer_Ready1 <= '0'; -- Realiza a leitura.	 
+				sig_rxread <= '1'; -- Realiza leitura
+				sig_txwrite <= '0'; -- Nao escreve
+				sig_txdata <= sig_rxdata;
+				
             when estado_escreve1 =>
-                TX_Write1 <= '1'; -- Realiza a escrita
- 				Buffer_Ready1 <= '1';
+--              TX_Write1 <= '1'; -- Realiza a escrita
+-- 				Buffer_Ready1 <= '1';
+				sig_rxread <= '0'; -- Para de fazer a leitura
+				sig_txwrite <= '1'; -- Realiza a escrita
+				
+				
 		end case;		  
 	end PROCESS;
 	
-
+-- ===============================================================================================
+-- = = = = = = = = = = = = = =   FIM PROCESSO LOOPBACK CODEC SpW   = = = = = = = = = = = = = = = =
+-- ===============================================================================================	
+	
 	PROCESS(RESET_doubleclk)
 	begin
 		if (not(RESET_doubleclk)='1') then
