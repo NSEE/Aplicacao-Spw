f40cc015351d8272983ab27ebfbfe0045113df9c
Dennis Teles <dennis.tds@hotmail.com>
2015-11-30 19:28:47 -0200

    diagrama de blocos do adc_controller corrigido e atualização de arquivos

--- Materiais/Codigos/Xilinx/Testes/Teste_loopback_SpW/Codec_SpW_loopback/_ngo/cs_ila_pro_0/ila_pro_0.vhd
+++ Materiais/Codigos/Xilinx/Testes/Teste_loopback_SpW/Codec_SpW_loopback/_ngo/cs_ila_pro_0/ila_pro_0.vhd
@@ -8,7 +8,7 @@
 -- \   \   \/     Version    : 14.7
 --  \   \         Application: XILINX CORE Generator
 --  /   /         Filename   : ila_pro_0.vhd
--- /___/   /\     Timestamp  : Wed Nov 18 21:26:21 Horário brasileiro de verão 2015
+-- /___/   /\     Timestamp  : Mon Nov 23 20:41:27 Horário brasileiro de verão 2015
 -- \   \  /  \
 --  \___\/\___\
 --
@@ -22,10 +22,12 @@ ENTITY ila_pro_0 IS
   port (
     CONTROL: inout std_logic_vector(35 downto 0);
     CLK: in std_logic;
-    TRIG0: in std_logic_vector(0 to 0);
+    TRIG0: in std_logic_vector(7 downto 0);
     TRIG1: in std_logic_vector(0 to 0);
     TRIG2: in std_logic_vector(0 to 0);
-    TRIG3: in std_logic_vector(0 to 0));
+    TRIG3: in std_logic_vector(7 downto 0);
+    TRIG4: in std_logic_vector(0 to 0);
+    TRIG5: in std_logic_vector(0 to 0));
 END ila_pro_0;
 
 ARCHITECTURE ila_pro_0_a OF ila_pro_0 IS
